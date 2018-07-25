c57f60dae8d6f2f5337deaf37936521c97b4b02f
Austin Hale <bbqdpickles@gmail.com>
2018-07-25 18:53:25 +0900

    Saving feature for DB, edit/save button fix

--- src/Resume/resume.go
+++ src/Resume/resume.go
@@ -3,11 +3,11 @@ package main
 import (
 	_ "github.com/mattn/go-sqlite3"
 	"database/sql"
-	"fmt"
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
 	"net/http"
 	"strconv"
+	"github.com/labstack/echo/middleware"
+	"fmt"
 )
 
 type PersonInfo struct {
@@ -18,26 +18,29 @@ type PersonInfo struct {
 }
 
 type Education struct {
-	id			 int		`db:"id" json:"id"`
+	ID			 int		`db:"id" json:"id"`
 	School       string   	`db:"school" json:"school"`
 	DateAttended string   	`db:"date_attended" json:"date_attended"`
 	Notes        []string 	`db:"notes" json:"notes"`
+	ResumeID	 int 		`db:"resume_id" json:"resume_id"`
 }
 
 type Employment struct {
-	id			 int		`db:"id" json:"id"`
+	ID			 int		`db:"id" json:"id"`
 	Company      string   	`db:"company" json:"company"`
 	DateAttended string   	`db:"date_attended" json:"date_attended"`
 	Position     string   	`db:"position" json:"position"`
 	Notes        []string 	`db:"notes" json:"notes"`
+	ResumeID	 int 		`db:"resume_id" json:"resume_id"`
 }
 
 type Volunteer struct {
-	id			 int		`db:"id" json:"id"`
+	ID			 int		`db:"id" json:"id"`
 	Company      string   	`db:"company" json:"company"`
 	DateAttended string   	`db:"date_attended" json:"date_attended"`
 	Position     string   	`db:"position" json:"position"`
 	Notes        []string 	`db:"notes" json:"notes"`
+	ResumeID	 int 		`db:"resume_id" json:"resume_id"`
 }
 
 type Resume struct {
@@ -48,102 +51,10 @@ type Resume struct {
 }
 
 var r = Resume{
-	PersonInfo: PersonInfo{ /*
-		Name:    "Austin Hale",
-		Address: "757 S 320 W, Provo, UT 84601",
-		Phone:   "+1-559-346-7123",
-		Email:   "austin.t.hale89@gmail.com",*/
-	},
-	Educations: []Education{ /*
-		{
-			School:       "Utah Valley University",
-			DateAttended: "Aug 2015 - Present",
-			Notes: []string{
-				"Cumulative GPA of 3.8, expected to graduate in Spring 2019.",
-				"Experienced in object-oriented and game development, design, testing, and debugging in teams and independently."},
-		},
-		{
-			School:       "BYU-Idaho",
-			DateAttended: "Apr 2011 – Dec 2012",
-			Notes: []string{
-				"Completed General Education classes and Intro to Programming classes"},
-		},*/
-	},
-	Employments: []Employment{ /*
-		{
-			Company:      "Utah Valley University CS Tutor Lab",
-			DateAttended: "Jan 2018 - Present",
-			Position:     "Computer Science Tutor and Grader",
-			Notes: []string{
-				"Coached students 1-on-1, teaching them to think critically for problem solving, debugging, and effective programming.",
-				"Facilitated numerous programming courses as a grader and class assistant, providing additional guidance and simplifying programming concepts (e.g. CPU Scheduling simulation, AVL Trees, Advanced Algorithms, etc.)",
-			},
-		},
-		{
-			Company:      "Frontier Communications",
-			DateAttended: "Feb 2016 – Aug 2016",
-			Position:     "Customer Service Representative",
-			Notes: []string{
-				"Averaged a 94 NPS by resolving customer concerns through explaining policies with professionalism and simplicity.",
-			},
-		}, */
-	},
-	Volunteers: []Volunteer{ /*
-		{
-			Company:      "Xing Zhou Bilingual School; Guangdong, China",
-			DateAttended: "Mar 2015 - Jul 2015",
-			Position:     "English Teacher",
-			Notes: []string{
-				"Adapted to Chinese culture while teaching English to 12 classes of 30+ students from 1st-8th grade",
-			},
-		},
-		{
-			Company:      "Church of Jesus Christ of Latter-day Saints; Salta, Argentina",
-			DateAttended: "Feb 2009 - Feb 2011",
-			Position:     "Full-time Representative",
-			Notes: []string{
-				"Organized and taught 10-60 minute lessons to individuals and families while mastering Spanish",
-				"Demonstrated leadership by planning service and community projects as Branch President",
-			},
-		}, */
-	},
-}
-
-type resume struct {
-	id 			int			`db:"id"`
-	full_name 	string		`db:"name" json:"name"`
-	address 	string		`db:"address"`
-	phone		string		`db:"phone"`
-	email		string		`db:"email"`
-}
-type education struct {
-	id			 int		`db:"id"`
-	school       string		`db:"school"`
-	DateAttended string		`db:"date_attended"`
-	Notes        []string	`db:"notes"`
-}
-
-type employment struct {
-	id			 int		`db:"id"`
-	Company      string		`db:"company"`
-	DateAttended string		`db:"date_attended"`
-	Position     string		`db:"position"`
-	Notes        []string
-}
-
-type volunteer struct {
-	id 			 int		`db:"id"`
-	Company      string		`db:"company"`
-	DateAttended string		`db:"date_attended"`
-	Position     string		`db:"position"`
-	Notes        []string	`db:"notes"`
-}
-
-type note struct {
-	id 			 int		`db:"id"`
-	section_id 	 int		`db:"section_id"`
-	detail 		 string		`db:"detail"`
-	note_type    int		`db:"note_type"`
+	PersonInfo: PersonInfo{},
+	Educations: []Education{},
+	Employments: []Employment{},
+	Volunteers: []Volunteer{},
 }
 
 //Initializes DB, returns a pointer to sql DB
@@ -170,7 +81,7 @@ func migrate(db *sql.DB) {
 		phone VARCHAR,
 		email VARCHAR
     );
-	DELETE FROM resume;
+	
 	CREATE TABLE IF NOT EXISTS education(
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		school TEXT,
@@ -178,7 +89,7 @@ func migrate(db *sql.DB) {
 		resume_id INTEGER,
 		FOREIGN KEY(resume_id) REFERENCES resume(id)
 	);
-	DELETE FROM education;
+	
 	CREATE TABLE IF NOT EXISTS employment(
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		company TEXT,
@@ -187,7 +98,7 @@ func migrate(db *sql.DB) {
 		resume_id INTEGER,
 		FOREIGN KEY(resume_id) REFERENCES resume(id)
 	);
-	DELETE FROM employment;
+	
 	CREATE TABLE IF NOT EXISTS volunteer(
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 		company TEXT,
@@ -196,78 +107,39 @@ func migrate(db *sql.DB) {
 		resume_id INTEGER,
 		FOREIGN KEY(resume_id) REFERENCES resume(id)
 	);
-	DELETE FROM volunteer;
-	CREATE TABLE IF NOT EXISTS note(
+	
+	CREATE TABLE IF NOT EXISTS edu_note(
 		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-		section_id INTEGER,
+		education_id INTEGER,
+		resume_id INTEGER,
 		detail TEXT,
-		type TEXT
+		FOREIGN KEY(education_id) REFERENCES education(id),
+		FOREIGN KEY(resume_id) REFERENCES resume(id)
 	);
-	DELETE FROM note;
-    `
+
+	CREATE TABLE IF NOT EXISTS emp_note(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		employment_id INTEGER,
+		resume_id INTEGER,
+		detail TEXT,
+		FOREIGN KEY(employment_id) REFERENCES employment(id),
+		FOREIGN KEY(resume_id) REFERENCES resume(id)
+	);
+
+	CREATE TABLE IF NOT EXISTS vol_note(
+		id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+		volunteer_id INTEGER,
+		resume_id INTEGER,
+		detail TEXT,
+		FOREIGN KEY(volunteer_id) REFERENCES volunteer(id),
+		FOREIGN KEY(resume_id) REFERENCES resume(id)
+	);
+	`
+
 	// Execute the SQL statement
 	_, err := db.Exec(sql)
 	// Exit if something goes wrong with our SQL statement above
 	checkErr(err)
-}
-
-func insertRecords(db *sql.DB) {
-	// Begin transaction
-	tx, err := db.Begin()
-	checkErr(err)
-	stmt, err := tx.Prepare("INSERT INTO resume(id, full_name, address, phone, email) VALUES(?, ?, ?, ?, ?)")
-	checkErr(err)
-	//defer stmt.Close()
-
-	// Create empty slice of resume struct pointers.
-	resumes := []*resume{}
-	resumes = append(resumes, &resume{id: 1, full_name: "Austin T. Hale", address: "123 Main st.", phone: "555-555-5555", email: "test@gmail.com"})
-
-	_, err = stmt.Exec(resumes[0].id, resumes[0].full_name, resumes[0].address, resumes[0].phone, resumes[0].email)
-	checkErr(err)
-
-	tx.Commit()
-	_, err = db.Exec("INSERT INTO education(id, school, date_attended, resume_id) values(1, 'UVU', 'Aug 2015 - Present',  1),(2, 'BYU-Idaho', 'Apr 2011 – Dec 2012', 1)")
-	_, err = db.Exec("INSERT INTO employment(id, company, position, date_attended, resume_id) values(1, 'UVU CS Tutor Lab', 'Tutor', 'Jan 2018 - Present',  1),(2, 'Frontier Communications', 'Customer Service Rep','Feb 2016 – Aug 2016', 1)")
-	_, err = db.Exec("INSERT INTO volunteer(id, company, position, date_attended, resume_id) values(1, 'Xing Zhou Bilingual School; Guangdong, China', 'English Teacher', 'Mar 2015 - Jul 2015',  1),(2, 'Church of Jesus Christ of Latter-day Saints', 'Missionary','Feb 2009 - Feb 2011', 1)")
-
-	_, err = db.Exec("INSERT INTO note(id, section_id, detail, type) values(1, 1, 'Cumulative GPA of 3.8, expected to graduate in Spring 2019.', 'Education'),(2, 1, 'Experienced in object-oriented and game development, design, testing, and debugging in teams and independently.', 'Education')")
-	_, err = db.Exec("INSERT INTO note(id, section_id, detail, type) values(3, 2, 'Completed General Education classes and Intro to Programming classes', 'Education'),(4, 1, 'Coached students 1-on-1, teaching them to think critically for problem solving, debugging, and effective programming.', 'Employment')")
-	_, err = db.Exec("INSERT INTO note(id, section_id, detail, type) values(5, 1, 'Facilitated numerous programming courses as a grader and class assistant, providing additional guidance and simplifying programming concepts (e.g. CPU Scheduling simulation, AVL Trees, Advanced Algorithms, etc.)', 'Employment'),(6, 2, 'Averaged a 94 NPS by resolving customer concerns through explaining policies with professionalism and simplicity.', 'Employment')")
-	_, err = db.Exec("INSERT INTO note(id, section_id, detail, type) values(7, 1, 'Adapted to Chinese culture while teaching English to 12 classes of 30+ students from 1st-8th grade', 'Volunteer'),(8, 2, 'Organized and taught 10-60 minute lessons to individuals and families while mastering Spanish', 'Volunteer')")
-	_, err = db.Exec("INSERT INTO note(id, section_id, detail, type) values(9, 2, 'Demonstrated leadership by planning service and community projects as Branch President', 'Volunteer')")
-}
-
-func readRecords(db *sql.DB) {
-	rows, err := db.Query("SELECT * from resume")
-	checkErr(err)
-	defer rows.Close()
-	for rows.Next() {
-		var id int
-		var name string
-		var address string
-		var phone string
-		var email string
-		err = rows.Scan(&id, &name, &address, &phone, &email)
-		checkErr(err)
-		fmt.Println(id, name, address, phone, email)
-	}
-	err = rows.Err()
-	checkErr(err)
-}
-
-
-func displayInfo(c echo.Context) error {
-	return c.JSON(http.StatusOK, r)
-}
-
-func saveInfo(c echo.Context) (err error) {
-	res := new(Resume)
-	if err = c.Bind(res); err != nil {
-		return
-	}
-	r = *res
-	return c.JSON(http.StatusOK, res) //changed r to res
 }
 
 func GetData(db *sql.DB) echo.HandlerFunc {
@@ -290,7 +162,7 @@ func GetData(db *sql.DB) echo.HandlerFunc {
 	r.Educations = getEduData(db)
 	r.Employments = getEmpData(db)
 	r.Volunteers = getVolData(db)
-	//log.Println(eduArray)
+
 	return func(c echo.Context) error {
 		return c.JSON(http.StatusOK, r)
 	}
@@ -303,6 +175,19 @@ func PutData(db *sql.DB) echo.HandlerFunc {
 			return
 		}
 		r = *res
+		// R now reflects the input that we used BIND to get, we can update the database
+		sql := "INSERT OR REPLACE INTO resume(id, full_name, address, phone, email) VALUES(?, ?, ?, ?, ?)"
+		// Create a prepared SQL statement
+		stmt, err := db.Prepare(sql)
+		checkErr(err)
+		// Replace the '?' in our prepared statement with values in our resume object
+		_, err2 := stmt.Exec(1, r.PersonInfo.Name, r.PersonInfo.Address, r.PersonInfo.Phone, r.PersonInfo.Email)
+		checkErr(err2)
+
+		setEduData(db)
+		setEmpData(db)
+		setVolData(db)
+
 		return c.JSON(http.StatusOK, res) //changed r to res
 	}
 }
@@ -320,15 +205,13 @@ func getEduData(db *sql.DB) []Education {
 		//var notes []string
 		err = eduRows.Scan(&id, &school, &dateAttended, &resumeID)
 		checkErr(err)
-		//eduDetails, err := db.Query("SELECT id, section_id, detail, type FROM notes WHERE type = 'Education' AND section_id = ?")
-
-		//notes = append(notes, )
 
 		//construct new Education object based on data we just read in, and add it to our array of Education objects
 		edu := Education{
-			id: id,
+			ID: id,
 			School: school,
 			DateAttended: dateAttended,
+			ResumeID: resumeID,
 		}
 
 		eduArray = append(eduArray, edu)
@@ -337,14 +220,15 @@ func getEduData(db *sql.DB) []Education {
 	// Get NOTES for each School section in Education
 	for idx, edu := range eduArray {
 		var notes []string
-		noteRows, err := db.Query("SELECT id, section_id, detail, type FROM note WHERE section_id = " + strconv.Itoa(edu.id) + " AND type = 'Education'")
+		noteRows, err := db.Query("SELECT id, education_id, detail, resume_id FROM edu_note WHERE education_id = " + strconv.Itoa(edu.ID) + " AND resume_id = " + strconv.Itoa(edu.ResumeID))
 		checkErr(err)
 		for noteRows.Next() {
 			var id int
 			var secID int
 			var txt string
-			var noteType string
-			err = noteRows.Scan(&id, &secID, &txt, &noteType)
+			var resID int
+			err = noteRows.Scan(&id, &secID, &txt, &resID)
+
 			checkErr(err)
 			notes = append(notes, txt)
 		}
@@ -365,27 +249,26 @@ func getEmpData(db *sql.DB) []Employment {
 		err = empRows.Scan(&id, &company, &dateAttended, &resumeID)
 		checkErr(err)
 
-		//notes = append(notes, )
-
 		//construct new Employment object based on data we just read in, and add it to our array of Employment objects
 		job := Employment{
-			id: id,
+			ID: id,
 			Company: company,
 			DateAttended: dateAttended,
+			ResumeID: resumeID,
 		}
 		empArray = append(empArray, job)
 	}
 	// Get NOTES for each Company section in Employment
 	for idx, job := range empArray {
 		var notes []string
-		noteRows, err := db.Query("SELECT id, section_id, detail, type FROM note WHERE section_id = " + strconv.Itoa(job.id) + " AND type = 'Employment'")
+		noteRows, err := db.Query("SELECT id, employment_id, detail, resume_id FROM emp_note WHERE employment_id = " + strconv.Itoa(job.ID) + " AND resume_id = " + strconv.Itoa(job.ResumeID))
 		checkErr(err)
 		for noteRows.Next() {
 			var id int
 			var secID int
 			var txt string
-			var noteType string
-			err = noteRows.Scan(&id, &secID, &txt, &noteType)
+			var resID int
+			err = noteRows.Scan(&id, &secID, &txt, &resID)
 			checkErr(err)
 			notes = append(notes, txt)
 		}
@@ -406,27 +289,26 @@ func getVolData(db *sql.DB) []Volunteer {
 		err = volRows.Scan(&id, &company, &dateAttended, &resumeID)
 		checkErr(err)
 
-		//notes = append(notes, )
-
 		//construct new Volunteer object based on data we just read in, and add it to our array of Volunteer objects
 		job := Volunteer{
-			id: id,
+			ID: id,
 			Company: company,
 			DateAttended: dateAttended,
+			ResumeID: resumeID,
 		}
 		volArray = append(volArray, job)
 	}
 	// Get NOTES for each Company section in Volunteer
 	for idx, job := range volArray {
 		var notes []string
-		noteRows, err := db.Query("SELECT id, section_id, detail, type FROM note WHERE section_id = " + strconv.Itoa(job.id) + " AND type = 'Employment'")
+		noteRows, err := db.Query("SELECT id, volunteer_id, detail, resume_id FROM vol_note WHERE volunteer_id = " + strconv.Itoa(job.ID) + " AND resume_id = " + strconv.Itoa(job.ResumeID))
 		checkErr(err)
 		for noteRows.Next() {
 			var id int
 			var secID int
 			var txt string
-			var noteType string
-			err = noteRows.Scan(&id, &secID, &txt, &noteType)
+			var resID int
+			err = noteRows.Scan(&id, &secID, &txt, &resID)
 			checkErr(err)
 			notes = append(notes, txt)
 		}
@@ -434,15 +316,68 @@ func getVolData(db *sql.DB) []Volunteer {
 	}
 	return volArray
 }
+
+func setEduData(db *sql.DB) {
+	//var thisID int
+	db.Exec("DELETE FROM education WHERE resume_id = 1")
+	for i, edu := range r.Educations {
+		sql := "INSERT INTO education(id, school, date_attended, resume_id) VALUES(?, ?, ?, ?)"
+		stmt, err := db.Prepare(sql)
+		checkErr(err)
+		_, err = stmt.Exec(i+1, edu.School, edu.DateAttended, 1)
+
+		//delete Note record for current education
+		db.Exec("DELETE FROM edu_note WHERE education_id = " + strconv.Itoa(i + 1))
+		for _, note := range edu.Notes {
+			sql := "INSERT INTO edu_note(education_id, resume_id, detail) VALUES(?, ?, ?)"
+			stmt, err := db.Prepare(sql)
+			checkErr(err)
+			_, err = stmt.Exec(i+1, 1, note)
+		}
+	}
+}
+func setEmpData(db *sql.DB) {
+	db.Exec("DELETE FROM employment WHERE resume_id = 1")
+	for i, job := range r.Employments {
+		sql := "INSERT INTO employment(id, company, date_attended, resume_id) VALUES(?, ?, ?, ?)"
+		stmt, err := db.Prepare(sql)
+		checkErr(err)
+		_, err = stmt.Exec(i+1, job.Company, job.DateAttended, 1)
+
+		//delete Note record for current education
+		db.Exec("DELETE FROM emp_note WHERE employment_id = " + strconv.Itoa(i + 1))
+		for _, note := range job.Notes {
+			sql := "INSERT INTO emp_note(employment_id, resume_id, detail) VALUES(?, ?, ?)"
+			stmt, err := db.Prepare(sql)
+			checkErr(err)
+			_, err = stmt.Exec(i+1, 1, note)
+		}
+	}
+}
+func setVolData(db *sql.DB) {
+	db.Exec("DELETE FROM volunteer WHERE resume_id = 1")
+	for i, job := range r.Volunteers {
+		sql := "INSERT INTO volunteer(id, company, date_attended, resume_id) VALUES(?, ?, ?, ?)"
+		stmt, err := db.Prepare(sql)
+		checkErr(err)
+		_, err = stmt.Exec(i+1, job.Company, job.DateAttended, 1)
+
+		//delete Note record for current education
+		db.Exec("DELETE FROM vol_note WHERE volunteer_id = " + strconv.Itoa(i + 1))
+		for _, note := range job.Notes {
+			sql := "INSERT INTO vol_note(volunteer_id, resume_id, detail) VALUES(?, ?, ?)"
+			stmt, err := db.Prepare(sql)
+			checkErr(err)
+			_, err = stmt.Exec(i+1, 1, note)
+		}
+	}
+}
 func main() {
 	// Initialize the DB
 	db := initDB("resume.db")
 	// close database connection before exiting program.
 	defer db.Close()
 	migrate(db)		// Schema migration
-
-	insertRecords(db)
-	readRecords(db)
 
 	// Echo instance
 	e := echo.New()
@@ -467,12 +402,10 @@ func main() {
 	e.Logger.Fatal(e.Start(":1323"))
 }
 
-
 func checkErr(err error, args ...string) {
 	if err != nil {
 		fmt.Println("Error")
 		fmt.Println(err, args)
-		//log.Fatal(err)
 		return
 	}
 }
